52b01ae3625cc66202c3c82ecc8cc3569e490224
Kartikeyan-rai <73011067+Kartikeyan-rai@users.noreply.github.com>
2021-08-17 16:35:26 +0530

    Add files via upload

---
+++ frute.js
@@ -0,0 +1,39 @@
+var status=""
+var object=[];
+function preload(){
+    img=loadImage('istockphoto-1067236648-1024x1024.jpg');
+}
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.position(450,120);
+    objectDetect=ml5.objectDetector('cocossd',modelLoaded)
+    document.getElementById("status").innerHTML="Status : detecting object"
+}
+function draw(){
+    image(img,0,0,500,500);
+    if(status != ""){ 
+        document.getElementById("status").innerHTML="Status : object detected";
+        for( i=0;i<object.length;i++){
+            fill("red");
+            confidenc=floor(object[i].confidence * 100)
+            text(object[i].label+" "+confidenc+"%",object[i].x+20,object[i].y+50);
+            noFill();
+            stroke("red");
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+        }
+    }
+}
+function modelLoaded(){
+    console.log("model loaded");
+    status=true;
+    objectDetect.detect(img , gotresults)
+}
+function gotresults(error,results){
+   if(error){
+       console.log(error);
+   }
+   else{
+       console.log(results);
+       object=results;
+   }
+}
---
+++ main.js
@@ -0,0 +1,12 @@
+function tvandac(){
+    window.open("frute.html")
+}
+function carandtruck(){
+    window.open("car and truck.html")
+}function dog_cat(){
+    window.open("dog and cat.html")
+}function bottel(){
+    window.open("bottel.html")
+}function bag(){
+    window.open("bag.html")
+}
